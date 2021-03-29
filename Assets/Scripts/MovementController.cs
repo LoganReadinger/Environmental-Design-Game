24d08d42d3cced7726a9bc99316dfd600a2da7e0
Logan <loganreadinger1@gmail.com>
2021-03-28 23:29:54 -0400

    Block Update
    Created block mockups for each level

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public double x = 1;
+    public double y = 1;
+    public double z = 1;
+
+    // Start is called before the first frame update
+    void Start(){
+            
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (){
+
+        }
+    }
+}
