0073e2a0af62fb0e203c3d7f244f096c77226016
Logan <loganreadinger1@gmail.com>
2021-04-01 03:00:11 -0400

    Nature Level
    -Finished nature level
    -Added scripts for the player

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -10,13 +10,10 @@ public class MovementController : MonoBehaviour
 
     // Start is called before the first frame update
     void Start(){
-            
+        
     }
 
     // Update is called once per frame
     void Update(){
-        if (){
-
-        }
     }
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour{
+    public float movementSpeed = 10;
+    public float jumpHeight = 1;
+
+    void Start(){
+        
+    }
+
+    void Update(){
+        
+    }
+}
